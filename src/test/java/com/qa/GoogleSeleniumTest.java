6613c4e3d2dc353c32d5c66f78c0608c260fc39b
tavi1994 <Mihai.Cernat@academytrainee.com>
2019-09-27 08:45:25 +0100

    added new button tests

--- src/test/java/com/qa/GoogleSeleniumTest.java
+++ src/test/java/com/qa/GoogleSeleniumTest.java
@@ -16,7 +16,7 @@ public class GoogleSeleniumTest {
 
     @Before
     public void setUp(){
-        System.setProperty("webdriver.chrome.driver","C:\\Users\\Admin\\IdeaProjects\\seleniumtesting\\src\\test\\java\\resources\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver","C:\\Users\\Admin\\Desktop\\seleniumtestingWorking\\src\\test\\java\\resources\\chromedriver.exe");
         driver = new ChromeDriver();
     }
 
@@ -28,32 +28,82 @@ public class GoogleSeleniumTest {
     @Test
     public void searchTest() throws InterruptedException {
         driver.manage().window().maximize();
-        driver.get("https://www.seleniumeasy.com/test/basic-first-form-demo.html");
-        Thread.sleep(1000);
+        driver.get("http://34.89.0.182/");
+        Thread.sleep(500);
 
-        WebElement inputField1 = driver.findElement(By.className("form-control"));
-        assertTrue(inputField1.isDisplayed());
-        WebElement submitButton = driver.findElement(By.xpath("//*[@id=\"get-input\"]/button"));
-
-        inputField1.sendKeys("Hello World!");
+//        WebElement inputField1 = driver.findElement(By.className("//*[@id=\"myCollection\"]/button"));
+//        assertTrue(inputField1.isDisplayed());
+        // inputField1.sendKeys("Hello World!");
+        WebElement submitButton = driver.findElement(By.xpath("//*[@id=\"myCollection\"]/button"));
         submitButton.click();
         Thread.sleep(5000);
 
-        WebElement inputFieldA = driver.findElement(By.id("sum1"));
-        WebElement inputFieldB = driver.findElement(By.id("sum2"));
-        assertTrue(inputFieldA.isDisplayed());
-        assertTrue(inputFieldB.isDisplayed());
-
-        JavascriptExecutor js = (JavascriptExecutor) driver;
-        js.executeScript("window.scrollBy(0,2000)");
-        Thread.sleep(1000);
-
-        inputFieldA.sendKeys("5");
-        inputFieldB.sendKeys("10");
-
-        WebElement submitButton2 = driver.findElement(By.xpath("//*[@id=\"gettotal\"]/button"));
-        submitButton2.click();
+        WebElement inputFieldLabel = driver.findElement(By.id("label"));
+        WebElement inputFieldTitle = driver.findElement(By.id("title"));
+        WebElement inputFieldArtist = driver.findElement(By.id("artist"));
+        WebElement inputFieldYear = driver.findElement(By.id("year"));
+        WebElement inputFieldValue= driver.findElement(By.id("value"));
+        assertTrue(inputFieldLabel.isDisplayed());
+        assertTrue(inputFieldTitle.isDisplayed());
+        assertTrue(inputFieldArtist.isDisplayed());
+        assertTrue(inputFieldYear.isDisplayed());
+        assertTrue(inputFieldValue.isDisplayed());
+        inputFieldLabel.sendKeys("BMY2020");
+        inputFieldArtist.sendKeys("Bob Marley");
+        inputFieldTitle.sendKeys("Exodus");
+        inputFieldYear.sendKeys("2001");
+        inputFieldValue.sendKeys("20");
         Thread.sleep(2000);
+
+        WebElement addButton = driver.findElement(By.xpath("//*[@id=\"addNewModal\"]/div/div/form/div/div[2]/button[2]"));
+        addButton.click();
+        Thread.sleep(2000);
+
+        WebElement updateButton = driver.findElement(By.className("btn-primary"));
+        updateButton.click();
+
+        Thread.sleep(1000);
+        WebElement updateFieldLabel = driver.findElement(By.id("label"));
+        WebElement updateFieldTitle = driver.findElement(By.id("title"));
+        WebElement updateFieldArtist = driver.findElement(By.id("artist"));
+        WebElement updateFieldYear = driver.findElement(By.id("year"));
+        WebElement updateFieldValue= driver.findElement(By.id("value"));
+        assertTrue(updateFieldLabel.isDisplayed());
+        assertTrue(updateFieldTitle.isDisplayed());
+        assertTrue(updateFieldArtist.isDisplayed());
+        assertTrue(updateFieldYear.isDisplayed());
+        assertTrue(updateFieldValue.isDisplayed());
+
+        updateFieldLabel.sendKeys("BOMA230");
+        updateFieldArtist.sendKeys("Kahn");
+        updateFieldTitle.sendKeys("Over Deh So");
+        updateFieldYear.sendKeys("2011");
+        updateFieldValue.sendKeys("26");
+        Thread.sleep(3000);
+
+        WebElement confUpdateButton = driver.findElement(By.xpath("//*[@id=\"updateModal\"]/div/div/form/div/div[2]/button[2]"));
+        confUpdateButton.click();
+        Thread.sleep(2000);
+
+        WebElement removeButton = driver.findElement(By.className("btn-danger"));
+        removeButton.click();
+        Thread.sleep(3000);
+
+
+
+        Thread.sleep(3000);
+
+
+//        JavascriptExecutor js = (JavascriptExecutor) driver;
+//        js.executeScript("window.scrollBy(0,2000)");
+//        Thread.sleep(1000);
+//
+//        inputFieldA.sendKeys("5");
+//        inputFieldB.sendKeys("10");
+//
+//        WebElement submitButton2 = driver.findElement(By.xpath("//*[@id=\"gettotal\"]/button"));
+//        submitButton2.click();
+//        Thread.sleep(2000);
 
 
 
